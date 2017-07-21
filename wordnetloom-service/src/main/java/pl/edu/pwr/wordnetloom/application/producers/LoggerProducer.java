5688a77176aae6ea1c09ed873f81c15ba1a94b31
Tomasz Naskręt <naskret.tomasz@gmail.com>
2017-07-21 17:07:43 +0200

    logging producer

--- wordnetloom-service/src/main/java/pl/edu/pwr/wordnetloom/application/producers/LoggerProducer.java
+++ wordnetloom-service/src/main/java/pl/edu/pwr/wordnetloom/application/producers/LoggerProducer.java
@@ -4,13 +4,10 @@ import java.util.logging.Logger;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
 
-/**
- * @author Tomasz Naskręt <naskret.tomasz@gmail.com>
- */
 public class LoggerProducer {
 
-    @Produces  
-    public Logger produceLogger(InjectionPoint injectionPoint) {  
-        return Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());  
-    } 
+    @Produces
+    public Logger produceLogger(InjectionPoint injectionPoint) {
+        return Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
+    }
 }
