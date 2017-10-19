fac003fa85a1840a70ecbf6e4fa21fb2661cb19a
Wojciech Rauk <wojtek.rauk@gmail.com>
2017-10-19 12:54:39 +0200

    deleted unnecessary comments

--- wordnetloom-web/src/main/front/src/app/services/http.service.ts
+++ wordnetloom-web/src/main/front/src/app/services/http.service.ts
@@ -8,9 +8,8 @@ import {SlimLoadingBarService} from 'ng2-slim-loading-bar';
 
 @Injectable()
 export class HttpService {
-  // apiBase = 'http://www.mocky.io/v2/';
-  apiBase = '/yiddish/api/';
-  // apiBase = 'http://156.17.135.29:8080/yiddish/api/';
+  // apiBase = '/yiddish/api/';
+  apiBase = 'http://156.17.135.29:8080/yiddish/api/';
 
   constructor(private http: Http, private slimLoadingBarService: SlimLoadingBarService) {}
 
@@ -30,8 +29,6 @@ export class HttpService {
   }
 
   getLexicalUnitDetails(id) {
-    // let uris = ['59d4bb9c270000d90607b435', '59d4bee1270000d90607b44e', '59d4beec270000bb0607b44f'];
-    // let uri = uris[Math.floor(Math.random() * uris.length)];
     return this.get('sense/' + id);
   }
 
@@ -43,10 +40,6 @@ export class HttpService {
       }
     }
     return this.get(searchStr + 'per_page=1000');
-  }
-
-  getDictOptions(searchedKey) {
-    return this.get('dictionary/' + searchedKey);
   }
 
   getGlobalOptions(searchedKey) {
