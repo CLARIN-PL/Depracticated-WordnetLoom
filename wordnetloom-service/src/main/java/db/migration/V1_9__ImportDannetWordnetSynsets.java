f65a03c2d2cd8a67c60cd9150b854036fa033a07
Mitchell John Seaton <seaton@hum.ku.dk>
2018-02-23 11:20:36 +0100

    dannet migrations

--- wordnetloom-service/src/main/java/db/migration/V1_9__ImportDannetWordnetSynsets.java
+++ wordnetloom-service/src/main/java/db/migration/V1_9__ImportDannetWordnetSynsets.java
@@ -113,7 +113,7 @@ public class V1_9__ImportDannetWordnetSynsets implements JdbcMigration {
                 if (id == f.syn_set_id) {
                     String comment = sa.getComment();
 
-                    if (comment != undefined && comment.length > 0)
+                    if (comment != undefined && comment.length() > 0)
                         sa.setComment(comment + '+' + f.ontological_type);
                     else
                         sa.setComment(f.ontological_type);
