e2475d56f9498533c422b8187de097d7a1384e93
Tomasz Naskręt <naskret.tomasz@gmail.com>
2018-04-17 11:54:10 +0200

    configuration for git version

--- wordnetloom-download/src/main/java/pl/edu/pwr/wordnetloom/business/boundary/DownloadResource.java
+++ wordnetloom-download/src/main/java/pl/edu/pwr/wordnetloom/business/boundary/DownloadResource.java
@@ -7,12 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.jar.Manifest;
 
 @Path("/")
@@ -21,21 +19,18 @@ public class DownloadResource {
     @Path("version")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getVersion(HttpServletRequest request) throws IOException {
+    public JsonObject getVersion(@Context HttpServletRequest request) throws IOException {
         ServletContext context = request.getSession().getServletContext();
         InputStream manifestStream = context.getResourceAsStream("/META-INF/MANIFEST.MF");
         Manifest manifest = new Manifest(manifestStream);
 
-        Map<String, String> response = new HashMap<>();
-        response.put("Implementation-Vendor", manifest.getMainAttributes().getValue("Implementation-Vendor"));
-        response.put("Implementation-Title", manifest.getMainAttributes().getValue("Implementation-Title"));
-        response.put("Implementation-Version", manifest.getMainAttributes().getValue("Implementation-Version"));
-        response.put("Implementation-Jdk", manifest.getMainAttributes().getValue("Build-Jdk"));
-        response.put("Implementation-Build", manifest.getMainAttributes().getValue("Implementation-Build"));
-        response.put("Implementation-Build-Time", manifest.getMainAttributes().getValue("Implementation-Build-Time"));
-
-
-        return Response.ok().entity(response).build();
+        return Json.createObjectBuilder()
+                .add("Implementation-Title", manifest.getMainAttributes().getValue("Implementation-Title"))
+                .add("Implementation-Version", manifest.getMainAttributes().getValue("Implementation-Version"))
+                .add("Implementation-Jdk", manifest.getMainAttributes().getValue("Build-Jdk"))
+                .add("Implementation-Build", manifest.getMainAttributes().getValue("Implementation-Build"))
+                .add("Implementation-Build-Time", manifest.getMainAttributes().getValue("Implementation-Build-Time"))
+                .build();
     }
 
     @Path("download")
