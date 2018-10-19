9f75d30764748dea2d10002fec088ff89b6d97be
Wojciech Rauk <wojtek.rauk@gmail.com>
2018-10-19 10:47:39 +0200

    finished language pipe

--- wordnetloom-web/src/main/front/src/app/pipes/translate.pipe.ts
+++ wordnetloom-web/src/main/front/src/app/pipes/translate.pipe.ts
@@ -1,18 +1,18 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 import {TranslateService} from '../services/translate.service';
 
 @Pipe({
-  name: 'translate'
+  name: 'translate',
+  pure: false
 })
 export class TranslatePipe implements PipeTransform {
 
   constructor(private translate: TranslateService) {}
 
   transform(key: any, args?: any): any {
-    // console.log(key);
-    // console.log(this.translate.data)
-    // return this.translate.get(key);
-    return this.translate.data[key] || key;
+    // console.log('transforming');
+    return this.translate.get(key);
+    // return this.translate.data[key] || key;
   }
 
 }
--- wordnetloom-web/src/main/front/src/app/services/translate.service.ts
+++ wordnetloom-web/src/main/front/src/app/services/translate.service.ts
@@ -1,23 +1,37 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpService} from './http.service';
-import {Observable} from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+
+@Injectable()
 export class TranslateService {
 
   data: any = {};
+  selectedLang = null;
+
+  cachedData = {};
 
   constructor(private http: HttpService) { }
 
   use(lang: string): Promise<{}> {
+    if (this.selectedLang === lang){
+      return;
+    }
+
+    this.selectedLang = lang;
+
+    // console.log(this.cachedData);
+
+    if (this.cachedData[this.selectedLang]) {
+        this.data = this.cachedData[this.selectedLang];
+        return;
+    }
+
     return new Promise<{}> ((resolve, reject) => {
       this.http.getLang(lang)
         .subscribe(
           translation => {
-            console.log(translation);
             this.data =  Object.assign({}, translation || {});
+            this.cachedData[lang] = this.data;
             resolve(this.data);
           },
           error => {
@@ -28,6 +42,6 @@ export class TranslateService {
   }
 
   get(key) {
-    return this.data[key] || key;
+      return this.data[key] || key;
   }
 }
